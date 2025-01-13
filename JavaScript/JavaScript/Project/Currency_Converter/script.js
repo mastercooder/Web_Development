a22b122fbe765c6335a706b63c0239ad7e879da4
mastercooder <tejesh.patel256@gmail.com>
2025-01-13 21:25:14 +0530

    13/01/2025

--- JavaScript/JavaScript/Project/Currency_Converter/script.js
+++ JavaScript/JavaScript/Project/Currency_Converter/script.js
@@ -2,6 +2,9 @@ const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
 
 for (let select of dropdowns){
     for(currCode in countryList){
@@ -27,4 +30,18 @@ const updateFlag = (element) =>{
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
-}
+};  
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1){
+        amtVal = 1;
+        amount.value = "1";
+    }
+    
+    // console.log(fromCurr.value, toCurr.value);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL)
+});
